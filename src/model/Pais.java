f98ffeb177e3c4c87aa471422bed69ded2f1ad14
Cancella <cancellacdc@gmail.com>
2023-10-11 13:56:22 -0300

    classe pais nova

--- src/model/Pais.java
+++ src/model/Pais.java
@@ -1,6 +1,41 @@
 package model;
 
 public class Pais {
-    Cartas.Territorio[] paises = Cartas.Territorio.allTerritorios;
+    private String nome;
+    private int tropas;
+    private Pais[] fronteiras;
+
+    public Pais(String nome) {
+        this.nome = nome;
+    }
+
+    public int getTropas() {
+        return tropas;
+    }
+
+    public void setTropas(int tropas) {
+        this.tropas = tropas;
+    }
+
+
+    Pais Angola = new Pais("Angola");
+    Pais Africa = new Pais("Africa");
+    Pais Madagascar = new Pais("Madagascar");
+
+    //Fronteiras
+    Angola.fronteiras =
+
 
 }
+
+//{
+// new Pais = Angola Pais("Angola")
+// new Pais = Brasil Pais("Brasil")
+// new Pais = Portugal Pais("Portugal")
+// ...
+
+// Angola ([Brasil, Portugal])
+
+// Brasil.fronteiras = {Chile, Argentina}
+// new Pais = Portugal Pais("Portugal")
+//}
