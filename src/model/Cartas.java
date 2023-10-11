2279dc1ccc00cc50f49a3da551c7dbb2a6d6fffd
Miguel Werneck <mig.roale@gmail.com>
2023-10-11 13:11:14 -0300

    mudei p krl filhoo

--- src/model/Cartas.java
+++ src/model/Cartas.java
@@ -29,10 +29,10 @@ class Cartas {
         triangulo
     }
     public static class Territorio {
-        private String pais;
-        private Poligono poligono;
+        private final String pais;
+        private final Poligono poligono;
 
-        private String[] fronteira; // usar .contains()
+        private final String[] fronteira; // usar .contains()
 
         public Territorio(String pais, Poligono poligono, String[] fronteira) {
             this.pais = pais;
@@ -48,7 +48,10 @@ class Cartas {
             return poligono;
         }
 
-        public Cartas.Territorio[] allTerritorios = {
+        public String[] getFronteira() {return fronteira;}
+
+
+        public static final Cartas.Territorio[] allTerritorios = {
                 new Cartas.Territorio("África do Sul", Cartas.Poligono.triangulo, new String[]{"Angola", "Somália"}),
                 new Cartas.Territorio("Angola", Cartas.Poligono.quadrado, new String[]{"África do Sul","Nigéria","Somália"}),
                 new Cartas.Territorio("Argélia", Cartas.Poligono.bola, new String[]{"Egito","Espanha","França","Itália","Nigéria"}),
@@ -106,35 +109,6 @@ class Cartas {
                 new Cartas.Territorio("Nova Zelândia", Poligono.quadrado, new String[]{"Austrália","Indonésia"}),
                 new Cartas.Territorio("Perth", Poligono.bola, new String[]{"Austrália"})
         };
-
-        public Cartas.Territorio[] listaTerritoriosAfrica = {
-                allTerritorios[0], allTerritorios[1], allTerritorios[2], allTerritorios[3], allTerritorios[4], allTerritorios[5]
-        };
-
-        public Cartas.Territorio[] ListaTerritoriosAmericaDoNorte = {
-                allTerritorios[6], allTerritorios[7], allTerritorios[8], allTerritorios[9], allTerritorios[10], allTerritorios[11],
-                allTerritorios[12],allTerritorios[13],allTerritorios[14]
-        };
-
-        public Cartas.Territorio[] ListaTerritoriosAsia = {
-                allTerritorios[15], allTerritorios[16], allTerritorios[17], allTerritorios[18], allTerritorios[19], allTerritorios[20],
-                allTerritorios[21], allTerritorios[22], allTerritorios[23], allTerritorios[24], allTerritorios[25], allTerritorios[26],
-                allTerritorios[27], allTerritorios[28], allTerritorios[29], allTerritorios[30], allTerritorios[31],
-                allTerritorios[32], allTerritorios[33], allTerritorios[34]
-        };
-        public Cartas.Territorio[] ListaTerritoriosAmericaDoSul = {
-                allTerritorios[35], allTerritorios[36], allTerritorios[37], allTerritorios[38],
-        };
-
-        public Cartas.Territorio[] ListaTerritoriosEuropa = {
-                allTerritorios[39], allTerritorios[40], allTerritorios[41], allTerritorios[42], allTerritorios[43], allTerritorios[44],
-                allTerritorios[45], allTerritorios[46]
-        };
-
-        public Cartas.Territorio[] ListaTerritoriosOceania = {
-                allTerritorios[47], allTerritorios[48], allTerritorios[49], allTerritorios[50]
-        };
-
 }
 }
 
---
+++ src/model/Pais.java
@@ -0,0 +1,6 @@
+package model;
+
+public class Pais {
+    Cartas.Territorio[] paises = Cartas.Territorio.allTerritorios;
+
+}
