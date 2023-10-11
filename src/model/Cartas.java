45d848e4f9923661da228d4691c14956368fb256
Miguel Werneck <mig.roale@gmail.com>
2023-10-11 10:44:26 -0300

    objetivos ajustes

--- src/model/Cartas.java
+++ src/model/Cartas.java
@@ -5,20 +5,20 @@ import java.util.List;
 class Cartas {
     // cada jogador tera um objetivo sorteado, ao objetivo ser sorteado, ele sera removido da lista, e outro sera sorteado
     public static String[] objetivo = { // 14 objetivos
-    "Destruir totalmente o jogador VERDE\nSe você é o VERDE, ou não há VERDE na partida, deve conquistar 24 terrítorios.",
-    "Destruir totalmente o jogador AZUL\nSe você é o AZUL, ou não há AZUL na partida, deve conquistar 24 terrítorios.",
-    "Destruir totalmente o jogador AMARELO\nSe você é o AMARELO, ou não há AMARELO na partida, deve conquistar 24 terrítorios.",
-    "Destruir totalmente o jogadoR VERMELHO\nSe você é o VERMELHO, ou não há VERMELHO na partida, deve conquistar 24 terrítorios.",
-    "Destruir totalmente o jogador PRETO\nSe você é o PRETO, ou não há PRETO na partida, deve conquistar 24 terrítorios.",
-     "Destruir totalmente o jogador BRANCO\nSe você é o BRANCO, ou não há BRANCO na partida, deve conquistar 24 terrítorios.",
-    "Conquistar por completo a ÁFRICA e a AMÉRICA DO NORTE",
-    "Conquistar por completo a ÁSIA e a ÁFRICA",
-    "Conquistar por completo a AMÉRICA DO NORTE  e a OCEANIA",
-    "Conquistar por completo a ÁSIA e a AMÉRICA DO SUL",
-    "Conquistar por completo a AMÉRICA DO SUL e EUROPA, e mais um continente de sua escolha",
-     "Conquistar por completo a EUROPA e a OCEANIA, e mais um continente de sua escolha",
-     "Conquistar 24 territórios de sua escolha",
-     "Conquistar 18 territórios e ocupar cada um deles com pelo menos 2 exércitos"
+    "Destruir totalmente o jogador VERDE\nSe você é o VERDE, ou não há VERDE na partida, ou se esses exercitos já foram destruidos por outro jogador, você deve conquistar 24 terrítorios.",
+    "Destruir totalmente o jogador AZUL\nSe você é o AZUL, ou não há AZUL na partida, ou se esses exercitos já foram destruidos por outro jogador, você deve conquistar 24 terrítorios.",
+    "Destruir totalmente o jogador AMARELO\nSe você é o AMARELO, ou não há AMARELO na partida, ou se esses exercitos já foram destruidos por outro jogador, você deve conquistar 24 terrítorios.",
+    "Destruir totalmente o jogadoR VERMELHO\nSe você é o VERMELHO, ou não há VERMELHO na partida, ou se esses exercitos já foram destruidos por outro jogador, você deve conquistar 24 terrítorios.",
+    "Destruir totalmente o jogador PRETO\nSe você é o PRETO, ou não há PRETO na partida, ou se esses exercitos já foram destruidos por outro jogador, você deve conquistar 24 terrítorios.",
+     "Destruir totalmente o jogador BRANCO\nSe você é o BRANCO, ou não há BRANCO na partida, ou se esses exercitos já foram destruidos por outro jogador, você deve conquistar 24 terrítorios.",
+    "Conquistar na totalidade a America do Norte e a Africa",
+    "Conquistar na totalidade a Asia e a Africa",
+    "Conquistar na totalidade a America do Norte e a Oceania",
+    "Conquistar na totalidade Asia e America do Sul",
+    "Conquistar na totalidade a Europa, a America do Sul e mais um continente a sua escolha",
+     "Conquistar na totalidade a Europa, a Oceania e mais um continente a sua escolha",
+     "Conquistar 24 territórios a sua escolha",
+     "Conquistar 18 territorios com pelo menos 2 exercitos em cada"
     };
     private Integer[] ListaCartasCoringa = {
         100, 100
